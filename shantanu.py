c4e346b416fed9dba4efcc6b30974d4c16c624e4
Ravee <raveendra.h@gmail.com>
2017-11-15 22:57:27 -0800

    Working code

--- shantanu.py
+++ shantanu.py
@@ -1,11 +1,14 @@
 import csv
 import re
 import time
+import os
 
-oneday = re.compile(r'(\w{3}) (\d{1,2}:\d{1,2} am|pm) - (\d{1,2}:\d{1,2} am|pm)')
-multiday = re.compile(r'(\w{3}-\w{3}) (\d{1,2}:\d{1,2} am|pm) - (\d{1,2}:\d{1,2} am|pm)')
+oneday = re.compile(r'(\w{3}) (\d{1,2}:\d{1,2}).(am|pm) - (\d{1,2}:\d{1,2}).(am|pm)')
+multiday = re.compile(r'(\w{3}-\w{3}) (\d{1,2}:\d{1,2}).(am|pm) - (\d{1,2}:\d{1,2}).(am|pm)')
+
 
 def readFile(filename):
+    """ Read the CSV file with raw dates """
     data = {}
     with open(filename) as f:
         reader = csv.reader(f)
@@ -18,35 +21,69 @@ def parseAvail(data):
     for k, v in data.items():
         v['proc'] = getAvail(v['raw'])
 
-    print(data)
-
 
 def getAvail(dateshit):
+    """ Convert raw available days to time object using re. Single and multiple days are considered
+        [(start, end), (start, end)] is structured format
+    """
     structured = []
     for item in dateshit.split(','):
+        item = item.strip()
         if oneday.match(item):
             groups = oneday.findall(item)[0]
-            starts = "{} {}".format(groups[0], groups[1])
-            ends = "{} {}".format(groups[0], groups[2])
+            starts = "{} {} {}".format(groups[0], groups[1], groups[2])
+            ends = "{} {} {}".format(groups[0], groups[3], groups[4])
 
             start = time.strptime(starts, "%a %I:%M %p")
             end = time.strptime(ends, "%a %I:%M %p")
             structured.append((start, end))
         elif multiday.match(item):
+            weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
             groups = multiday.findall(item)[0]
-            days = groups[0].split('-')
 
-            starts = "{} {}".format(days[0], groups[1])
-            ends = "{} {}".format(days[1], groups[2])
+            availrange = groups[0].split('-')
+            availdays = weekdays[weekdays.index(availrange[0].strip()):weekdays.index(availrange[-1].strip()) + 1]
 
-            start = time.strptime(starts, "%a %I:%M %p")
-            end = time.strptime(ends, "%a %I:%M %p")
-            structured.append((start, end))
+            for day in availdays:
+                starts = "{} {} {}".format(day, groups[1], groups[2])
+                ends = "{} {} {}".format(day, groups[3], groups[4])
+
+                start = time.strptime(starts, "%a %I:%M %p")
+                end = time.strptime(ends, "%a %I:%M %p")
+                structured.append((start, end))
     return structured
 
 
+def getSlot(dateinput):
+    """ Read the user input slot and convert to time objects """
+    slot = []
+    inputf = dateinput.split('-')
+    slot.extend([inputf[0][:3], inputf[0][3:].strip(), inputf[1].strip()])
+
+    starts = "{} {}".format(slot[0], slot[1])
+    ends = "{} {}".format(slot[0], slot[2])
+
+    start = time.strptime(starts, "%a %I:%M %p")
+    end = time.strptime(ends, "%a %I:%M %p")
+
+    return start, end
+
+
+def findAavail(pdata, dateinput):
+    """ Loop through each reps formatted availability and look for match """
+    slot = getSlot(dateinput)
+    avail = []
+    for k, v in pdata.items():
+        for proc in v['proc']:
+            if slot[0] >= proc[0] and slot[1] <= proc[1]:
+                avail.append(k)
+    return avail
+
+
 if __name__ == '__main__':
-    file = '/Users/ravee/Downloads/sdr_availability.csv'
+    file = os.getenv('HOME') + '/Downloads/sdr_availability.csv'
     data = readFile(file)
-    pdata = parseAvail(data)
-    # dateinput = input("Enter Day and Time:")
+    parseAvail(data)
+    dateinput = input("Enter Day and Time Range (Ex: Mon 9:00 am - 10:30 pm):")
+    reps = findAavail(data, dateinput.strip())
+    print('Available Reps:', ', '.join(reps))
