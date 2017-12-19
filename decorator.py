64f91bf9a4a3702ccd9dfb9db6bcd1e5e713b3fb
Ravee <raveendra.h@gmail.com>
2017-12-19 10:44:04 -0800

    decorator with argument

--- decorator.py
+++ decorator.py
@@ -3,9 +3,21 @@ def dec_4x(func):
         return func(*args, **kwargs) * 4
     return wrapper
 
+def multipy_nx(n):
+    def dec_nx(func):
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs) * n
+        return wrapper
+    return dec_nx
+
 @dec_4x
 def add_two(num):
     return num + 2
 
+@multipy_nx(2)
+def add_three(num):
+    return num + 3
+
 if __name__ == '__main__':
     print(add_two(10))
+    print(add_three(10))
